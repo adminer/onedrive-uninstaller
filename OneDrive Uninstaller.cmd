6d5076d3d20dac4e8b982dc802e4014ea180413c
Thomas Chappelow <hello@chappelow.eu>
2016-01-24 18:16:03 +0000

    v1.0
    Minor descriptive changes.

--- OneDrive Uninstaller.cmd
+++ OneDrive Uninstaller.cmd
@@ -4,7 +4,7 @@ cls
 set x86="%SYSTEMROOT%\System32\OneDriveSetup.exe"
 set x64="%SYSTEMROOT%\SysWOW64\OneDriveSetup.exe"
 
-echo Closing OneDrive process.
+echo Closing the OneDrive process.
 echo.
 taskkill /f /im OneDrive.exe > NUL 2>&1
 ping 127.0.0.1 -n 5 > NUL 2>&1
@@ -25,7 +25,7 @@ rd "C:\OneDriveTemp" /Q /S > NUL 2>&1
 rd "%LOCALAPPDATA%\Microsoft\OneDrive" /Q /S > NUL 2>&1
 rd "%PROGRAMDATA%\Microsoft OneDrive" /Q /S > NUL 2>&1 
 
-echo Removing OneDrive from the Explorer Side Panel...
+echo Removing OneDrive from the Windows Explorer Side Panel...
 echo.
 REG DELETE "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > NUL 2>&1
 REG DELETE "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > NUL 2>&1
